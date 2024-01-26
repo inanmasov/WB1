31f8f1a8c3ef40d353d682e3377eb00bf90030fd
inanmasov <inanmasov2017@mail.ru>
2024-01-26 16:59:03 +0300

    Сorrections

--- service/internal/cached/cached.go
+++ service/internal/cached/cached.go
@@ -50,31 +50,3 @@ func NewCache(defaultExpiration, cleanupInterval time.Duration) *CacheManager {
 		Cache: cache.New(defaultExpiration, cleanupInterval),
 	}
 }
-
-// Set устанавливает значение в кэше с заданным ключом и сроком действия.
-func (cm *CacheManager) Set(key string, value interface{}, expiration time.Duration) {
-	cm.Mu.Lock()
-	defer cm.Mu.Unlock()
-	cm.Cache.Set(key, value, expiration)
-}
-
-// Get получает значение из кэша по ключу.
-func (cm *CacheManager) Get(key string) (interface{}, bool) {
-	cm.Mu.Lock()
-	defer cm.Mu.Unlock()
-	return cm.Cache.Get(key)
-}
-
-// Delete удаляет значение из кэша по ключу.
-func (cm *CacheManager) Delete(key string) {
-	cm.Mu.Lock()
-	defer cm.Mu.Unlock()
-	cm.Cache.Delete(key)
-}
-
-// Flush очищает весь кэш.
-func (cm *CacheManager) Flush() {
-	cm.Mu.Lock()
-	defer cm.Mu.Unlock()
-	cm.Cache.Flush()
-}
